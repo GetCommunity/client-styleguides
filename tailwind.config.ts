a8eed933f177a84b44b8e0b9c606b01d739bcd94
Joey Grable <hello@joeygrable.com>
2026-04-03 09:55:48 -0700

    chore: updated dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,31 @@
-import {
-  ClientStylesBIASC,
-  ClientStylesBrandywine,
-  ClientStylesGetCommunity,
-  ClientStylesOlsonhomes,
-  ClientStylesPacificcommunities,
-  ClientStylesSheahomes,
-  ClientStylesSocialBrands,
-  ClientStylesTollbrothers,
-  ClientStylesTrumark,
-  ClientStylesVandaele,
-  ClientStylesWoodbridgePacificGroup,
-} from '@getcommunity/client-styleguides';
+import ClientStyleguideBiasc from "@getcommunity/client-styleguides/biasc"
+import ClientStyleguideBrandywine from "@getcommunity/client-styleguides/brandywine"
+import ClientStyleguideGetcommunity from "@getcommunity/client-styleguides/getcommunity"
+import ClientStyleguideOlsonhomes from "@getcommunity/client-styleguides/olsonhomes"
+import ClientStyleguidePacificcommunities from "@getcommunity/client-styleguides/pacificcommunities"
+import ClientStyleguideSheahomes from "@getcommunity/client-styleguides/sheahomes"
+import ClientStyleguideSocialBrands from "@getcommunity/client-styleguides/social-brands"
+import ClientStyleguideTollbrothers from "@getcommunity/client-styleguides/tollbrothers"
+import ClientStyleguideTrumark from "@getcommunity/client-styleguides/trumark"
+import ClientStyleguideVandaele from "@getcommunity/client-styleguides/vandaele"
+import ClientStyleguideWpg from "@getcommunity/client-styleguides/wpg"
 
 export default {
-  content: ['./dev/*.html', './dev/**/*.{js,jsx,ts,tsx}'],
+  content: ["./dev/*.html", "./dev/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    ClientStylesBIASC,
-    ClientStylesBrandywine,
-    ClientStylesGetCommunity,
-    ClientStylesOlsonhomes,
-    ClientStylesPacificcommunities,
-    ClientStylesSheahomes,
-    ClientStylesSocialBrands,
-    ClientStylesTollbrothers,
-    ClientStylesTrumark,
-    ClientStylesVandaele,
-    ClientStylesWoodbridgePacificGroup,
-  ],
-};
+    ClientStyleguideBiasc,
+    ClientStyleguideBrandywine,
+    ClientStyleguideGetcommunity,
+    ClientStyleguideOlsonhomes,
+    ClientStyleguidePacificcommunities,
+    ClientStyleguideSheahomes,
+    ClientStyleguideSocialBrands,
+    ClientStyleguideTollbrothers,
+    ClientStyleguideTrumark,
+    ClientStyleguideVandaele,
+    ClientStyleguideWpg
+  ]
+}
